5d4d2f2ae54057b9366044fb5cb001669e4735cd
Tran Chien <vjpboy03td@gmail.com>
2026-04-25 21:45:32 +0700

    JS - Operator(+, -, *, /, %) & Precedence

---
+++ lesson_02/main.js
@@ -0,0 +1,10 @@
+// Các phép toán cơ bản + - * / %
+var numberA = 6,
+  numberB = 5;
+console.log("Tổng 2 số", numberA, "và", numberB, "là", numberA + numberB);
+console.log(`Hiệu 2 số ${numberA} và ${numberB} là ${numberA - numberB}`);
+console.log(`Nhân 2 số ${numberA} và ${numberB} là ${numberA * numberB}`);
+console.log(numberA / numberB);
+console.log(numberA % numberB);
+// Right to left
+let x = (y = 25);
